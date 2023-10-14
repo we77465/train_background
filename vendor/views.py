0cb5f546cfe267335cfbb1c3ddd2987d03c1b00d
we77465 <68180138+we77465@users.noreply.github.com>
2023-10-14 11:54:17 +0800

    錯誤的

--- vendor/views.py
+++ vendor/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render
 from .models import Vendor
 # Create your views here.
 def showtemplate(request):
-    vendor_list = Vendor.objects.all() # 把所有 Vendor 的資料取出來
-    context = {'vendor_list': vendor_list} # 建立 Dict對應到Vendor的資料，
-    return render(request, 'test.html', context)
+    vendor_list = Vendor.objects.all()
+    context = {'vendor_list': vendor_list}
+    # print(vendor_list)
+    return render(request, 'vendors/vendor_detail.html', context)
+
+
+from .forms import VendorForm # 要記得 import 相對應的 Model Form 唷!
+
+# 針對 vendor_create.html
+def vendor_create_view(request):
+    form = VendorForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = VendorForm()
+
+    context = {
+        'form' : form
+    }
+    return render(request, "vendors/vendor_create.html", context)
