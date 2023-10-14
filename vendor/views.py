1dbcfc5a296b129be7750a156d2656dad192b4aa
we77465 <68180138+we77465@users.noreply.github.com>
2023-10-14 13:45:11 +0800

    1014
    終於修好了 是dir的[]要保留

--- vendor/views.py
+++ vendor/views.py
@@ -3,7 +3,8 @@ from .models import Vendor
 
 # Create your views here.
 def showtemplate(request):
-    #vendor_list = Vendor.objects.all() # 把所有 Vendor 的資料取出來
-    #context = {'vendor_list': vendor_list} # 建立 Dict對應到Vendor的資料，
+
+    vendor_list = Vendor.objects.all() # 把所有 Vendor 的資料取出來
+    context = {'vendor_list': vendor_list} # 建立 Dict對應到Vendor的資料，
 
     return render(request, 'detail.html')
